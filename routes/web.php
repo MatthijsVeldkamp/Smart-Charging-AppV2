71a346b6acb8001e7a71d4fbb03151df47a4310c
Matthijs Veldkamp <99071402@mydavinci.nl>
2025-01-16 13:44:25 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/sockets', function () {
 })->middleware(['auth', 'verified'])->name('sockets');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', [ProfileController::class, '         edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
