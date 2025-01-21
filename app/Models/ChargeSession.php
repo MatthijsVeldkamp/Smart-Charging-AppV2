85862f0483cfb73afa70fcb99c0888b1ab229357
Matthijs Veldkamp <99071402@mydavinci.nl>
2025-01-21 12:48:16 +0100

    socketsessie

---
+++ app/Models/ChargeSession.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChargeSession extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'socket_id',
+        'time_begin', 
+        'time_end',
+        'power_consumption',
+        'created_at',
+        'updated_at'
+    ];
+}
